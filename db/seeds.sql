498a56415d0d8ea8e6205a2d0ce5a491b5847f2f
Robert Bolton <therobshock@yahoo.com>
2019-05-07 11:49:23 -0700

    database

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Bovinegeist", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Stinky Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("In N Out Doggystyle", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Big and Nasty", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("California Not-a-burger", false);
